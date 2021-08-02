3d6030eb9b7127d67326ae62d5be90a41e701190
Syed Rahaman <syed.rahaman@kartbites.com>
2021-08-02 14:50:59 +0530

    kill process on host destroy

--- android/src/main/java/com/voximplant/foregroundservice/VIForegroundServiceModule.java
+++ android/src/main/java/com/voximplant/foregroundservice/VIForegroundServiceModule.java
@@ -123,6 +123,8 @@ public class VIForegroundServiceModule extends ReactContextBaseJavaModule implem
             Intent intent = new Intent(getReactApplicationContext(), VIForegroundService.class);
             intent.setAction(Constants.ACTION_FOREGROUND_SERVICE_STOP);
             getReactApplicationContext().stopService(intent);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(0);
         }
 
     }
